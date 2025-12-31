c8a4ebb94d1fb577d3b3796bffd0ee51875c5bc3
whx_xx <3243916556@qq.com>
2025-12-31 13:58:52 +0800

    建立前端文件目录结构，添加一个空的pagecontroller

--- oj_tracker/src/main/java/hdc/rjxy/controller/PageController.java
+++ oj_tracker/src/main/java/hdc/rjxy/controller/PageController.java
@@ -6,19 +6,4 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
-    @GetMapping("/login")
-    public String loginPage() {
-        return "login"; // 对应 login.html
-    }
-
-    @GetMapping("/me")
-    public String mePage() {
-        return "me";    // 对应 me.html
-    }
-
-    // 如果有主页
-    @GetMapping("/")
-    public String indexPage() {
-        return "redirect:/me"; // 暂时跳到个人中心
-    }
 }
